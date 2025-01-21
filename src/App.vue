77775b34e5d27f49032b5854cc95d530b1a3e73f
Raziel Ludovico Hatzke <raziel_ludovico.hatzke@smail.th-koeln.de>
2025-01-21 21:45:11 +0100

    Added mock settings page

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,15 @@
 <script setup>
 
-// TODO:
-// Chart (esp donut) refactor for reusablity
-// basic html layout (what goes where in relation to what etc) for all routed components
-
-// Router Screens:
-// Lifetime Frequency Chart + Lifetime Donut
-// Settings (simple lists with setting ideas for now)
-
 </script>
 
 <template>
   <nav>
     <ul>
-      <li><router-link to="/">Hobbies </router-link></li>
-      <li><router-link to="/calendar">Calendar </router-link></li>
-      <li><router-link to="/stats">Statistics </router-link></li>
-      <li><router-link to="/suggestions">Suggestions </router-link></li>
+      <li><router-link to="/">Hobbies</router-link></li>
+      <li><router-link to="/calendar">Calendar</router-link></li>
+      <li><router-link to="/stats">Statistics</router-link></li>
+      <li><router-link to="/suggestions">Suggestions</router-link></li>
+      <li><router-link to="/settings">Settings</router-link></li>
     </ul>
   </nav>
     <router-view />
---
+++ src/components/Settings.vue
@@ -0,0 +1,17 @@
+<script setup>
+//TODO:
+//Everything
+</script>
+<template>
+  <h4>Visual</h4>
+  <li>
+    <ul>Theme Toggle</ul>
+    <ul>High Contrast Mode</ul>
+  </li>
+  <h4>Data</h4>
+  <li>
+    <ul>Export Data</ul>
+    <ul>Import Data</ul>
+    <ul>Reset All Data</ul>
+  </li>
+</template>
