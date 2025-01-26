51c6826ab946da2be2ae2176ac8eb6d73fc2f475
Raziel Ludovico Hatzke <raziel_ludovico.hatzke@smail.th-koeln.de>
2025-01-26 19:49:45 +0100

    Refactored main logic for Distribution Chart into own component for reusablity

--- src/components/QuickStatistics.vue
+++ src/components/QuickStatistics.vue
@@ -45,60 +45,6 @@ const topChartOptions = {
   }
 };
 
-const distributionChartData = computed(() => {
-  const sortedHobbies = filteredHobbies.value.slice().sort((a, b) => a.hobbyHistory.length - b.hobbyHistory.length);
-  const labels = sortedHobbies.map(hobby => hobby.text);
-  const data = sortedHobbies.map(hobby => hobby.hobbyHistory.length);
-
-  const backgroundColors = generateColors(data.length);
-
-  return {
-    labels,
-    datasets: [
-      {
-        data: data,
-        backgroundColor: backgroundColors,
-        borderColor: 'rgb(36, 36, 36)', //LATER: change according to theme
-        borderWidth: 2,
-      },
-    ],
-  };
-});
-
-const distributionChartOptions = {
-  responsive: true,
-  maintainAspectRatio: false,
-  plugins: {
-    legend: { 
-      display: true,
-      position: 'bottom',
-      labels: {
-        usePointStyle: true,
-        generateLabels: (chart) => {
-          const data = chart.data.datasets[0];
-          return chart.data.labels.map((label, i) => ({
-            text: `${label} (${data.data[i]})`,
-            fillStyle: data.backgroundColor[i],
-            fontColor: 'rgb(96, 96, 96)', //LATER: adjust based on theme
-            pointStyle: 'circle',
-            lineWidth: 0,
-          }));
-        },
-      },
-    },
-    tooltip: { enabled: false }
-  },
-};
-
-const generateColors = (count) => {
-  const colors = [];
-  for (let i = 0; i < count; i++) {
-    const hue = 360 - (i * 90) / (count - 1);
-    colors.push(`hsla(${hue}, 70%, 50%, 0.8)`);
-  }
-  return colors;
-};
-
 </script>
 
 <template>
@@ -110,7 +56,7 @@ const generateColors = (count) => {
       </TopHobbiesChart>
     </div>
     <div style="width: 400px;">
-      <DistributionChart :distributionChartData="distributionChartData" :distributionChartOptions="distributionChartOptions">
+      <DistributionChart :chartInput="filteredHobbies">
         Chart couldn't render.
       </DistributionChart>
     </div>
