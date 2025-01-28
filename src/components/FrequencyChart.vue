3b4bda8837424c66a5713876aaded09732fd8816
Raziel Ludovico Hatzke <raziel_ludovico.hatzke@smail.th-koeln.de>
2025-01-28 15:38:27 +0100

    Refactored majority of chart logic from FrequencyStatistics to FrequencyChart

--- src/components/FrequencyChart.vue
+++ src/components/FrequencyChart.vue
@@ -1,19 +1,40 @@
 <script setup>
 
+import { computed } from 'vue';
 import { Bar } from 'vue-chartjs';
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
 
-defineProps({
-  chartData: {
+const props = defineProps({
+  chartInput: {
     type: Object,
     required: true,
   },
-  chartOptions: {
-    type: Object,
-    required: true,
-  },
+});
+
+const chartOptions = {
+  responsive: true,
+  maintainAspectRatio: false,
+  plugins: {
+    legend: { display: false },
+    tooltip: { enabled: false }
+  }
+};
+
+const chartData = computed(() => {
+  const labels = [...props.chartInput].map(hobby => hobby.text);
+  const data = [...props.chartInput].map(hobby => hobby.hobbyHistory.length);
+  return {
+    labels,
+    datasets: [
+      {
+        label: 'Hobby Activity',
+        data,
+        backgroundColor: 'rgba(245, 40, 145, 0.8)'
+      }
+    ]
+  };
 });
 
 </script>
--- src/components/FrequencyStatistics.vue
+++ src/components/FrequencyStatistics.vue
@@ -1,35 +1,10 @@
 <script setup>
 
-import { computed } from 'vue'
 import { useHobbiesStore } from '../stores/hobbies';
 import FrequencyChart from './FrequencyChart.vue';
 import DistributionChart from './DistributionChart.vue';
 
 const hobbies = useHobbiesStore();
-
-const frequencyChartData = computed(() => {
-  const labels = hobbies.hobbies.map(hobby => hobby.text);
-  const data = hobbies.hobbies.map(hobby => hobby.hobbyHistory.length);
-  return {
-    labels,
-    datasets: [
-      {
-        label: 'Hobby Activity',
-        data,
-        backgroundColor: 'rgba(245, 40, 145, 0.8)'
-      }
-    ]
-  };
-});
-
-const frequencyChartOptions = {
-  responsive: true,
-  maintainAspectRatio: false,
-  plugins: {
-    legend: { display: false },
-    tooltip: { enabled: false }
-  }
-};
 
 function removeHobbyHistory() {
   if (confirm('Are you sure you want to reset your hobby history data? This cannot be undone.')) {
@@ -44,7 +19,7 @@ function removeHobbyHistory() {
     <h3>Statistics</h3>
     <button @click="removeHobbyHistory">Delete all Frequency Data.</button>
     <div style="height: 24rem;width: 37.5rem">
-      <FrequencyChart :chartData="frequencyChartData" :chartOptions="frequencyChartOptions">
+      <FrequencyChart :chartInput="hobbies.hobbies">
         Chart couldn't render
       </FrequencyChart>
     </div>
